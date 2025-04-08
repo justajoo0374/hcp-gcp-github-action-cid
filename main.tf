0789e152137dac463fe6219de7a616ae2b140b57
justajoo0374 <justajoo0374@gmail.com>
2025-04-08 10:45:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_storage_bucket" "my-bucket" {
 }
 
 resource "google_storage_bucket" "my-bucket25" {
-  name          = "bkt-demo-002"
+  name          = "bkt-demo-0025"
   location      = "us-central1"
   project = "magnetic-flare-454316-h0"
   force_destroy = true
